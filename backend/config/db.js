38829e0f903e6638932de1b76cd4c110dc5cc23d
EngHussein71 <you@example.com>
2026-02-09 11:25:04 -0800

    xusen

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 const dotenv = require('dotenv');
 
 dotenv.config();
-
+// prapering the connuction to connuct
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
